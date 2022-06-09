2adff6548d1c2ee1cb00541a125f153ca5de0f9c
LorellW <lorellwendel@gmail.com>
2022-06-09 12:30:12 +0300

    security_admin6/9/22

--- src/main/java/com/github/lorellw/letscode/entiites/Role.java
+++ src/main/java/com/github/lorellw/letscode/entiites/Role.java
@@ -3,7 +3,7 @@ package com.github.lorellw.letscode.entiites;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {

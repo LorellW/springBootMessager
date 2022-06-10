be63e82308f09b9338ee3ada4669009c14667bee
LorellW <lorellwendel@gmail.com>
2022-06-10 10:06:37 +0300

    Upload_files
    png with russian chracters in the name is not displayed

--- src/main/java/com/github/lorellw/letscode/controllers/RootController.java
+++ src/main/java/com/github/lorellw/letscode/controllers/RootController.java
@@ -3,18 +3,26 @@ package com.github.lorellw.letscode.controllers;
 import com.github.lorellw.letscode.entiites.Message;
 import com.github.lorellw.letscode.entiites.User;
 import com.github.lorellw.letscode.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class RootController {
 
     private final MessageRepository messageRepository;
+    @Value("${upload.path}")
+    private String uploadPath;
 
     public RootController(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
@@ -30,11 +38,11 @@ public class RootController {
         Iterable<Message> messages;
         if (filter != null && !filter.isEmpty()) {
             messages = messageRepository.findByTag(filter);
-        }else {
-            messages =messageRepository.findAll();
+        } else {
+            messages = messageRepository.findAll();
         }
         model.addAttribute("messages", messages);
-        model.addAttribute("filter",filter);
+        model.addAttribute("filter", filter);
         return "root";
     }
 
@@ -42,12 +50,28 @@ public class RootController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (!file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
-        return "root";
+        return "redirect:/root";
     }
 
 }
--- src/main/resources/templates/parts/common.ftl
+++ src/main/resources/templates/parts/common.ftl
@@ -3,6 +3,7 @@
 <head>
     <title>Spring boot messager</title>
     <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="/static/css/style.css">
 </head>
 <body>
 <#nested>
